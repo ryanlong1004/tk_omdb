7ec9f75376b0bb561bad5f793ee76c50c39263dd
Ryan Long <rlong@wasabi.com>
2025-02-22 20:03:08 -0500

    updates

--- src/omdb.py
+++ src/omdb.py
@@ -8,7 +8,7 @@ load_dotenv()
 api_key = os.getenv("OMDB_API_KEY")
 
 
-def get_movie_details(title: str, year: str = "") -> dict:
+def get_movie_by_title_year(title: str, year: str = "") -> dict:
     url = f"https://www.omdbapi.com/?apikey={api_key}&t={title}&y={year}"
     response = requests.get(url, timeout=10)
     return response.json()
@@ -21,13 +21,13 @@ def get_movie_details_by_id(imdb_id: str) -> dict:
 
 
 def search_movies_by_title(title: str) -> dict:
-    url = f"https://www.omdbapi.com/?apikey={api_key}&s={title}"
+    url = f"https://www.omdbapi.com/?apikey={api_key}&s={title}"q
     response = requests.get(url, timeout=10)
     return response.json()
 
 
 if __name__ == "__main__":
-    movie_details = get_movie_details("DeadStream", "2022")
+    movie_details = get_movie_by_title_year("DeadStream", "2022")
     print(json.dumps(movie_details, indent=4))
 
     movie_details_by_id = get_movie_details_by_id("tt1234567")
